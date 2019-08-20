adeb82702c3c8c95c37016fcaad01bbeea03319e
lkmarcum <50113705+lkmarcum@users.noreply.github.com>
2019-08-20 15:00:40 -0500

    set up axiosWithAuth call to server

--- friends/src/components/Friends.js
+++ friends/src/components/Friends.js
@@ -1,6 +1,25 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { axiosWithAuth } from "../utils/axiosWithAuth";
 
 const Friends = props => {
+  const [friends, setFriends] = useState([]);
+
+  useEffect(() => {
+    getFriends();
+  }, []);
+
+  const getFriends = () => {
+    axiosWithAuth()
+      .get(`http://localhost:5000/api/friends`)
+      .then(res => {
+        console.log("friends res: ", res);
+        setFriends(res.data);
+      })
+      .catch(err => {
+        console.log(err.response);
+      });
+  };
+
   return <h1>Friends Page</h1>;
 };
 
