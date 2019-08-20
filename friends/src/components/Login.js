9800abdbe480c0538bec1783a467a1516df2e2e5
lkmarcum <50113705+lkmarcum@users.noreply.github.com>
2019-08-20 14:48:19 -0500

    set up login page & logic

--- friends/src/components/Login.js
+++ friends/src/components/Login.js
@@ -1,7 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 
 const Login = props => {
-  return <h1>Login Page</h1>;
+  const [credentials, setCredentials] = useState({
+    username: "",
+    password: ""
+  });
+
+  const handleChange = e => {
+    setCredentials({ ...credentials, [e.target.name]: e.target.value });
+  };
+
+  const login = e => {
+    e.preventDefault();
+    axios
+      .post(`http://localhost:5000/api/login`, credentials)
+      .then(res => {
+        localStorage.setItem("token", res.data.payload);
+        props.history.push("/friends");
+      })
+      .catch(err => {
+        console.log(err.response);
+      });
+    setCredentials({ username: "", password: "" });
+  };
+
+  return (
+    <div>
+      <form onSubmit={login}>
+        <input
+          type="text"
+          name="username"
+          value={credentials.username}
+          onChange={handleChange}
+        />
+        <input
+          type="password"
+          name="password"
+          value={credentials.password}
+          onChange={handleChange}
+        />
+        <button>Log In</button>
+      </form>
+    </div>
+  );
 };
 
 export default Login;
